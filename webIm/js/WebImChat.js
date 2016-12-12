c34cea53c0ee83e666234ab99ce83955719015a5
jql <719712166@qq.com>
2016-12-12 22:49:35 +0800

    客户端游客随机数不能大于9位

--- webIm/js/WebImChat.js
+++ webIm/js/WebImChat.js
@@ -32,7 +32,8 @@ function getRandomString()
 	var Rand = Math.random();   
 	var num=10 + Math.round(Rand * Range);
 	var date = new Date();
-	var time=date.getTime();
+	var time=date.getTime()+"";
+	time=time.substr(0,4);
 	return (time+""+num);
 }
 
